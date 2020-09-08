98789b5a3e67bab3c465d33d95837a2542400d62
Lui22 <Leorybalov@ya.ru>
2020-09-08 22:15:13 +0500

    Updates

---
+++ precache-manifest.96d7777790081c7201bc727588ccb1f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb678cbc4ae93311e6ed24e7605c343",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffbfc76c819967b7ea73",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d1becd128e9babd3c594",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ffbfc76c819967b7ea73",
+    "url": "./static/js/2.f716bffe.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.f716bffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1becd128e9babd3c594",
+    "url": "./static/js/main.c7a2d281.chunk.js"
+  },
+  {
+    "revision": "b287ee4749fabaf3dc16",
+    "url": "./static/js/runtime-main.e88f8eff.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
